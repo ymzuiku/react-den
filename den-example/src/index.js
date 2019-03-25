09e6f6c08bcb37aa14e634485be73416406edd7c
梁柱 <liangzhu@77ircloud.com>
2019-03-26 01:23:07 +0800

    remove dll

--- den-example/src/index.js
+++ den-example/src/index.js
@@ -1,12 +1,5 @@
-import 'modules/pures/normal.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { unregister } from 'modules/serviceWorker';
 import Home from 'src/Home';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-unregister();
